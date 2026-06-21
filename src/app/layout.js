131a7e17130bca7ad49f451ffe597d5ea352d753
onamikaonu086-boop <onamikaonu086@gmail.com>
2026-06-21 23:52:40 +0600

    make the navbar and footer fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,14 +2,18 @@
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { AuthProvider } from '@/context/AuthContext';
 import "./globals.css";
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body suppressHydrationWarning={true} className="...">
         <GoogleOAuthProvider clientId="204458447857-hocqbhhnmgb9tmarc0edh5fq7179tc09.apps.googleusercontent.com">
           <AuthProvider>
+            <Navbar />
             {children}
+            <Footer/>
           </AuthProvider>
         </GoogleOAuthProvider>
       </body>
